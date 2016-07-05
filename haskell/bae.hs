404e54703a52de8b5daa31f73f5130b617827071
Perry Alexander <palexand@ku.edu>
2016-07-05 11:39:01 -0500

    FAE now uses GADT notation

--- haskell/bae.hs
+++ haskell/bae.hs
@@ -10,23 +10,20 @@ import ParserUtils
 
 -- Calculator language extended with an environment to hold defined variables
 
-data BAE = Num Int
-         | Plus BAE BAE
-         | Minus BAE BAE
-         | Mult BAE BAE
-         | Div BAE BAE
-         | Bind String BAE BAE
-         | Id String
-  deriving Show
+data BAE where
+  Num :: Int -> BAE
+  Plus :: BAE -> BAE -> BAE
+  Minus :: BAE -> BAE -> BAE
+  Bind :: String -> BAE -> BAE -> BAE
+  Id :: String -> BAE
+  deriving (Show,Eq)
 
 -- Parser
 
 expr :: Parser BAE
 expr = buildExpressionParser operators term
 
-operators = [ [ inFix "*" Mult AssocLeft
-              , inFix "/" Div AssocLeft ]
-            , [ inFix "+" Plus AssocLeft
+operators = [ [ inFix "+" Plus AssocLeft
               , inFix "-" Minus AssocLeft ]
             ]
 
@@ -62,8 +59,6 @@ subst :: String -> BAE -> BAE -> BAE
 subst _ _ (Num x) = (Num x)
 subst i v (Plus l r) = (Plus (subst i v l) (subst i v r))
 subst i v (Minus l r) = (Minus (subst i v l) (subst i v r))
-subst i v (Mult l r) = (Mult (subst i v l) (subst i v r))
-subst i v (Div l r) = (Div (subst i v l) (subst i v r))
 subst i v (Bind i' v' b') = if i==i'
                             then (Bind i' (subst i v v') b')
                             else (Bind i' (subst i v v') (subst i v b'))
@@ -75,8 +70,6 @@ calcs :: BAE -> Int
 calcs (Num x) = x
 calcs (Plus l r) = (calcs l) + (calcs r)
 calcs (Minus l r) = (calcs l) - (calcs r)
-calcs (Mult l r) = (calcs l) * (calcs r)
-calcs (Div l r) = div (calcs l) (calcs r)
 calcs (Bind i v b) = (calcs (subst i (Num (calcs v)) b))
 calcs (Id id) = error "Undeclared Variable"
 
@@ -88,8 +81,6 @@ calc :: BAE -> Env -> Int
 calc (Num x) env = x
 calc (Plus l r) env = (calc l env) + (calc r env)
 calc (Minus l r) env = (calc l env) - (calc r env)
-calc (Mult l r) env = (calc l env) * (calc r env)
-calc (Div l r) env = div (calc l env) (calc r env)
 calc (Bind i v b) env =
   let v' = calc v env in
     calc b ((i,v'):env)

858b5c9d482c405ed09e1525811b5d8e12650b34
Perry Alexander <palexand@ku.edu>
2018-01-23 18:05:29 -0600

    adding project 0 template

--- haskell/ae.hs
+++ haskell/ae.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, FlexibleContexts #-}
 
 -- Imports for QuickCheck
 import System.Random
@@ -13,9 +13,6 @@ import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Language
 import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Token
-
--- Imports for PLIH
-import ParserUtils
 
 --
 -- Simple caculator over naturals with no identifiers
@@ -34,6 +31,7 @@ data AE where
   Minus :: AE -> AE -> AE
   Mult :: AE -> AE -> AE
   Div :: AE -> AE -> AE
+  If0 :: AE -> AE -> AE -> AE
   deriving (Show,Eq)
 
 -- Lift a function over integers into Num
@@ -42,32 +40,76 @@ liftNum :: (Int -> Int -> Int) -> AE -> AE -> AE
 liftNum f (Num l) (Num r) = (Num (f l r))
 
 -- AST Pretty Printer
+-- Prints concrete syntax from abstract syntax
 
 pprintAE :: AE -> String
 pprintAE (Num n) = show n
 pprintAE (Plus n m) = "(" ++ pprintAE n ++ "+" ++ pprintAE m ++ ")"
 pprintAE (Minus n m) = "(" ++ pprintAE n ++ "-" ++ pprintAE m ++ ")"
-
---instance Show AE where
---  show = pprintAE
+pprintAE (Mult n m) = "(" ++ pprintAE n ++ "*" ++ pprintAE m ++ ")"
+pprintAE (Div n m) = "(" ++ pprintAE n ++ "/" ++ pprintAE m ++ ")"
+pprintAE (If0 c t e) = "(if0 " ++ pprintAE c ++ " then " ++ pprintAE t ++ " else " ++ pprintAE e ++ ")"
 
 -- Parser (Requires ParserUtils and Parsec)
+
+languageDef =
+  javaStyle { identStart = letter
+            , identLetter = alphaNum
+            , reservedNames = [ "if0"
+                              , "then"
+                              , "else"
+                              ]
+            , reservedOpNames = [ "+","-","*","/"]
+            }
+  
+lexer = makeTokenParser languageDef
+
+inFix o c a = (Infix (reservedOp lexer o >> return c) a)
+preFix o c = (Prefix (reservedOp lexer o >> return c))
+postFix o c = (Postfix (reservedOp lexer o >> return c))
+
+parseString p str =
+  case parse p "" str of
+    Left e -> error $ show e
+    Right r -> r
+
+parseFile p file =
+  do program <- readFile file
+     case parse p "" program of
+       Left e -> print e >> fail "parse error"
+       Right r -> return r
 
 expr :: Parser AE
 expr = buildExpressionParser operators term
 
-operators = [ [ inFix "+" Plus AssocLeft
-              , inFix "-" Minus AssocLeft ]
-            ]
-
+operators = [
+  [ inFix "*" Mult AssocLeft
+    , inFix "/" Div AssocLeft ]
+  , [ inFix "+" Plus AssocLeft
+  , inFix "-" Minus AssocLeft ]
+  ]
+  
 numExpr :: Parser AE
 numExpr = do i <- integer lexer
              return (Num (fromInteger i))
 
+ifExpr :: Parser AE
+ifExpr  = do reserved lexer "if0"
+             c <- expr
+             reserved lexer "then"
+             t <- expr
+             reserved lexer "else"
+             e <- expr
+             return (If0 c t e)
+                     
+
 term = parens lexer expr
        <|> numExpr
+       <|> ifExpr
 
 -- Parser invocation
+-- Call parseAE to parse a string into the AE data structure.  Call
+-- parseAEFile to parse a comlete file.
 
 parseAE = parseString expr
 
@@ -88,7 +130,9 @@ eval (Mult t1 t2) = do v1 <- (eval t1)
                        return (liftNum (*) v1 v2)
 eval (Div t1 t2) = do v1 <- (eval t1)
                       v2 <- (eval t2)
-                      return (liftNum div v1 v2)
+                      if v2==(Num 0) then Nothing else return (liftNum div v1 v2)
+eval (If0 t1 t2 t3) = do v1 <- (eval t1)
+                         (if (v1 == (Num 0)) then (eval t2) else (eval t3))
 
 -- Interpreter = parse + eval
 
@@ -116,12 +160,33 @@ genMinus n =
      t <- genAE n
      return (Minus s t)
 
+genMult n =
+  do s <- genAE n
+     t <- genAE n
+     return (Minus s t)
+
+genDiv n =
+  do s <- genAE n
+     t <- genAE n
+     return (Mult s t)
+
+genIf n =
+  do c <- genAE n
+     t <- genAE n
+     e <- genAE n
+     return (If0 c t e)
+
 genAE :: Int -> Gen AE
 genAE 0 =
   do term <- genNum
      return term
 genAE n =
-  do term <- oneof [genNum,(genPlus (n-1)),(genMinus (n-1))]
+  do term <- oneof [genNum
+                   , (genPlus (n-1))
+                   , (genMinus (n-1))
+                   , (genMult (n-1))
+                   , (genDiv (n-1))
+                   , (genIf (n-1))]
      return term
 
 -- QuickCheck 
@@ -130,7 +195,7 @@ testParseAE :: Int -> IO ()
 testParseAE n = quickCheckWith stdArgs {maxSuccess=n}
   (\t -> parseAE (pprintAE t) == t)
 
-testInterp :: Int -> IO ()
-testInterp n = quickCheckWith stdArgs {maxSuccess=n}
+testInterpAE :: Int -> IO ()
+testInterpAE n = quickCheckWith stdArgs {maxSuccess=n}
   (\t -> (interp $ pprintAE t) == (eval t))
 
